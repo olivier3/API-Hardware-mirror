fea51dcb9ed6d2c98bfc01498bd3ab6027ad27bc
Olivier <olivierbilodeau@outlook.com>
2023-11-29 10:57:03 -0500

    feat: add basic webSocket conection + communication

--- Program.cs
+++ Program.cs
@@ -7,13 +7,41 @@ var app = builder.Build();
 // Set the API port that it listen on
 app.Urls.Add("http://*:15000");
 
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await WS.Echo(webSocket);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    else
+    {
+        await next(context);
+    }
+
+});
+
 app.MapPost("/updateTemp",
     (HouseDataDb db, HttpContext httpContext) => Esp.UpdateTemp(db, httpContext));
 
-app.MapPost("/updateHumidity", 
+app.MapPost("/updateHumidity",
     (HouseDataDb db, HttpContext httpContext) => Esp.UpdateHumidity(db, httpContext));
 
-app.MapPost("/link", 
+app.MapPost("/link",
     (HouseDataDb db, HttpContext httpContext) => Mirror.LinkMirror(db, httpContext));
 
 app.Run();
